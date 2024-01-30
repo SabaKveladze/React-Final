f0680ea8f660270df8c668512cf74c601636d39b
Saba Kveladze <sabaqveladze16@gmail.com>
2024-01-31 01:45:28 +0400

    Added Search Function

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,51 +1,75 @@
 import './style.css'
 import Header from './components/Header/Header'
-import cart from './img/cart.png'
-import { useEffect,useState } from 'react'
-import {WatchCard} from './components/WatchCard/WatchCard'
-import Footer from './components/Footer/Footer'
+
+import { useEffect, useState } from 'react'
+import { WatchCard } from './components/WatchCard/WatchCard'
+
 import axios from 'axios'
 
 const Main = () => {
-    const [email, setEmail  ] = useState("");
-    const [data, setData] = useState([]);
+  const [email, setEmail] = useState("");
+  const [data, setData] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
   useEffect(() => {
     axios
       .get("http://localhost:3031/watches")
-      .then((data) => setData(data.data));
+      .then((response) => setData(response.data));
   }, []);
 
+  useEffect(() => {
+    if (inputValue === "") {
+      axios
+        .get("http://localhost:3031/watches")
+        .then((response) => setData(response.data));
+    } else {
+      const filteredData = data.filter((watch) =>
+        watch.name.toLowerCase().includes(inputValue.toLowerCase())
+      );
+      setData(filteredData);
+    }
+  }, [inputValue]);
 
-    return (   
+  return (
     <div>
-
-
-       <Header />
-        <h5>Click On Watch Photo To See Its Details!</h5>
-    <section className="featured-watches">
-        
+      <Header />
+      <h5>Click On Watch Photo To See Its Details!</h5>
+      <input
+        placeholder='Search'
+        className='search'
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <section className="featured-watches">
         {
-            data.map((data) => (<WatchCard data={data}/> ))
+          data.map((watch, index) => (<WatchCard key={index} data={watch}/> ))
         }
-        
-    </section>
-
-    <section className="newsletter">
+      </section>
+      <section className="newsletter">
         <h2>Subscribe to Our Newsletter</h2>
         <form>
-           
-            <input type="email" value={email} onChange=  {(e) => setEmail(e.target.value)} name="email" placeholder="Your email" required/>
-            <button className='subBtn' type="submit" onClick={() => {setEmail(""); alert("Thank you for your subscribtion") } }>Subscribe</button>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            placeholder="Your email"
+            required
+          />
+          <button
+            className='subBtn'
+            type="submit"
+            onClick={() => {setEmail(""); alert("Thank you for your subscription") }}
+          >
+            Subscribe
+          </button>
         </form>
-    </section>
-
-    <footer>
+      </section>
+      <footer>
         <p>&copy; 2024 Watches Store. All rights reserved.</p>
-    </footer>
-</div>
-    
-
- )
+      </footer>
+    </div>
+  )
 }
 
 export default Main
--- src/components/WatchCard/WatchCard.jsx
+++ src/components/WatchCard/WatchCard.jsx
@@ -1,4 +1,4 @@
-import { Navigate } from 'react-router-dom'
+
 import './watchcard.css'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
